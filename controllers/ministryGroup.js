5f7e702b5734353c6da878ec3fea0eacc4baaf45
Glitch (territorymanager-api) <none>
2025-07-07 23:30:31 +0200

    delete pdf libraries

--- controllers/ministryGroup.js
+++ controllers/ministryGroup.js
@@ -3,58 +3,11 @@ import MinistryGroup from "../models/ministryGroup.js";
 import Preacher from "../models/preacher.js";
 import flash from "connect-flash";
 import methodOverride from "method-override";
-import ejs from 'ejs';
-import pdf from 'html-pdf';
-import path from 'path';
 import { __dirname } from "../app.js";
 const app = express();
 
 app.use(flash());
 app.use(methodOverride("_method"))
-
-export const generateListOfMinistryGroups = (req, res, next) => {
-    MinistryGroup
-        .find({congregation: req.user._id})
-        .populate(["preachers", "overseer"])
-        .sort({name: 1})
-        .exec()
-        .then((ministryGroups) => {
-            let data = {
-                currentUser: req.user.toJSON(), 
-                ministryGroups
-            };
-            ejs.renderFile(path.join(__dirname, './views/ministryGroups/generate-pdf.ejs'), data, {}, function(err, str) {
-                if (err) return res.send({...err, line: 28});
-
-                const title = data.currentUser.username.split(" ").join("-");
-                const DOWNLOAD_DIR = path.join(process.env.HOME || process.env.USERPROFILE, 'downloads/')
-
-
-                var options = { format: 'A4', orientation: 'landscape', timeout: 540000 };
-
-                pdf.create(str, options).toFile(`${DOWNLOAD_DIR}Grupy_sluzby_${title}.pdf`, function(err, data) {
-                    if (err) return console.log(err);
-                  
-                    
-                
-                   res.download(data.filename, `Grupy_sluzby_${title}.pdf`, (err) => {
-                        if (err) {
-                            res.send({
-                                error : err,
-                                msg   : "Problem downloading the file"
-                            })
-                        } else {
-                            res.send("Plik pomyślnie utworzony. Zobacz folder Pobrane")
-                    
-                        }
-                    })
-                });
-            
-            });
-            
-        })
-        .catch((err) => console.log(err))
-}
 
 export const getListOfMinistryGroups = (req, res, next) => {
     const congregationID = req.user.username ? String(req.user._id) : req.user.congregation;
